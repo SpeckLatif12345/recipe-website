e4cb91633e9d55d1e0c9bdfcdad60db1a680353f
Abdul Latif Suley <122109392+SpeckLatif12345@users.noreply.github.com>
2025-04-26 09:03:28 +0000

    Add files via upload

---
+++ recipe-website/script.js
@@ -0,0 +1,12 @@
+let leftButton = document.getElementById("nav-rightBtn");
+let links = document.getElementById("links-container");
+
+leftButton.addEventListener("click", function () {
+  if (links.classList.contains("showLinks")) {
+    links.classList.remove("showLinks");
+    return;
+  } else links.classList.add("showLinks");
+  {
+    return;
+  }
+});
